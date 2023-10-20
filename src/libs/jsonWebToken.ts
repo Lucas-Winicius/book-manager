2cf315f8159b646f50119c27a5f4d94dc9f51708
Lucas-Winicius <lucasclucas7@gmail.com>
2023-10-20 17:59:37 -0300

    authorization middleware

--- src/libs/jsonWebToken.ts
+++ src/libs/jsonWebToken.ts
@@ -1,9 +1,20 @@
 import jwt, { Secret } from "jsonwebtoken";
+import UserType from "../@types/UserType";
 
 class JsonWebToken {
   create(data: object | string | Buffer) {
     const secretOrPrivateKey = process.env.JWT_SECRET as Secret;
     return jwt.sign(data, secretOrPrivateKey, { expiresIn: "7d" });
+  }
+
+  verify(token: string){
+    try {
+      const secretOrPrivateKey = process.env.JWT_SECRET as Secret;
+      const decoded = jwt.verify(token, secretOrPrivateKey) as UserType;
+      return decoded;
+    } catch (err) {
+      return null;
+    }
   }
 }
 
