e16fe499320f69764eda5d982e1d6b24357a4240
Lucas-Winicius <lucasclucas7@gmail.com>
2023-10-20 17:59:28 -0300

    authorization middleware

---
+++ src/middlewares/Authentication.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Request, Response } from "express";
+import jsonWebToken from "../libs/jsonWebToken";
+import { error } from "../libs/responses";
+
+function Authentication(req: Request, res: Response, next: NextFunction) {
+  const token = req.headers.authorization?.split(" ")[1] || "";
+  const user = jsonWebToken.verify(token);
+
+  if (!user) {
+    const response = error({
+      status: 401,
+      message: `Access to this resource is unauthorized. Please ensure you have the necessary permissions and credentials to access this content. If you believe this is in error, please contact our support team for assistance.`,
+    });
+
+    return res.status(response.status).json(response);
+  }
+
+  next();
+}
+
+export default Authentication;
